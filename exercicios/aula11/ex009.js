a24b4e63f02cb5164f938450086392bd443a5e9c
Leonardo Barbosa <leobarbosajf@hotmail.com>
2022-06-11 15:25:08 -0300

    Criação da pasta aula 11 e exs

---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //CONDIÇÃO SIMPLES, QUANDO NÃO TEM UM SENÃO
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
+
