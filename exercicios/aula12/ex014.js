2c4e304e2f910a1633a8c94351558375cc63b95c
Leonardo Barbosa <leobarbosajf@hotmail.com>
2022-06-17 17:04:11 -0300

    concluindo ex014

--- exercicios/aula12/ex014.js
+++ exercicios/aula12/ex014.js
@@ -1,10 +1,12 @@
 var agora = new Date()
 var hora = agora.getHours()//PEGA A HORA ATUAL DO SISTEMA EM QUE O SCRIPT ESTÁ RODANDO.
-console.log(`Agora são exatamente ${hora} horas.`)
-if(hora < 12){
-    console.log('Bom Dia')
-}else if(hora >= 12 && hora <= 18){
-    console.log('Boa Tarde')
-}else{
-    console.log('Boa Noite')
+console.log(`Agora são exatamente ${hora}.`)
+if(hora > 0 && hora < 6){
+    console.log('Boa Madrugada!')
+} else if(hora < 12){//hora >= 6 NÃO PRECISO ESCREVER POIS NA CONDIÇÃO DE CIMA FALA QUE A hora < 6 ENTAO AQUI SÓ PODE SER >=6.
+    console.log('Bom Dia!')
+} else if(hora <= 18){
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
 }
