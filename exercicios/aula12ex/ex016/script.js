38a310b85b30957b36799ece457a241af280300a
Leonardo Barbosa <leobarbosajf@hotmail.com>
2022-07-01 22:57:11 -0300

    comecei ex17

--- exercicios/aula12ex/ex016/script.js
+++ exercicios/aula12ex/ex016/script.js
@@ -3,7 +3,7 @@ function carregar(){
     var imagem = document.querySelector('img#imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 18
+    //var hora = 15
     var min = data.getMinutes()
     //var horacomp = String(hora) + ':' + String(min)
     mensagem.innerHTML = `Agora são ${hora}:${min}`
@@ -15,13 +15,10 @@ function carregar(){
     }else if(hora >= 12 && hora < 18){
         //BOA TARDE
         imagem.src = 'fototarde.png'
-        document.body.style.background = '#804b43'
+        document.body.style.background = '#fdcd9d'
     }else{
         //BOA NOITE
         imagem.src = 'fotonoite.png'
         document.body.style.background = '#0c2134'
     }
-    
-
-
 }
