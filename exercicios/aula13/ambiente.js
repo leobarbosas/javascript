17f3f122d721eb44fd86ef7e68ae27fad94c3f62
Leonardo Barbosa <leobarbosajf@hotmail.com>
2022-07-20 21:57:29 -0300

    criação pastas ex013

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/* var c = 1
+while(c <= 6){
+    console.log(`Passo ${c*}`)
+    c++ //c = c + 1
+} */
+
+/* console.log('Todo bem?')
+console.log('Todo bem?')
+console.log('Todo bem?')
+console.log('Todo bem?')
+console.log('Todo bem?')
+console.log('Todo bem?') */
